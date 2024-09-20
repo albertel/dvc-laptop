65b23ca321afcc5584e09306b84cd3b67742f8bb
albertel <guy@albertelli.com>
2024-09-20 15:31:47 -0700

    Update Do-LaptopConfigure.ps1

--- Do-LaptopConfigure.ps1
+++ Do-LaptopConfigure.ps1
@@ -4,6 +4,7 @@
 #  would be better if we examined the possible screen resolutions and picked one rather then tryign a bunch from a list
 
 Set-StrictMode -version latest
+"Running version 1"
 
 Function Set-ScreenResolution { 
 
