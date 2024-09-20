dcc2ed7114e288eb508af8656bd120fb3b2f5b93
albertel <guy@albertelli.com>
2024-09-20 16:40:49 -0700

    Update Do-LaptopConfigure.ps1
    Make the remove of no file not error

--- Do-LaptopConfigure.ps1
+++ Do-LaptopConfigure.ps1
@@ -209,7 +209,7 @@ Function UpdateOrCreate-ItemProperty($path, $name, $value, $propertytype) {
 # MAIN
 
 # Set up a scheduled task on Logon to ask some input and download and run the branched version.
-$args='-command "Set-ExecutionPolicy -Force:$true -ExecutionPolicy RemoteSigned; cd \Users\Student\Downloads; rm Do-LaptopConfigure.ps1;Invoke-WebRequest -Uri https://raw.githubusercontent.com/albertel/dvc-laptop/refs/heads/'+$branch+'/Do-LaptopConfigure.ps1 -OutFile Do-LaptopConfigure.ps1; .\Do-LaptopConfigure.ps1;Read-Host second_yo"'
+$args='-command "Set-ExecutionPolicy -Force:$true -ExecutionPolicy RemoteSigned; cd \Users\Student\Downloads; rm  -ErrorAction Ignore Do-LaptopConfigure.ps1;Invoke-WebRequest -Uri https://raw.githubusercontent.com/albertel/dvc-laptop/refs/heads/'+$branch+'/Do-LaptopConfigure.ps1 -OutFile Do-LaptopConfigure.ps1; .\Do-LaptopConfigure.ps1;Read-Host second_yo"'
 $taskName = "LaptopConfigure"
 $createTask = $true
 $task=Get-ScheduledTask | Where {$_.TaskName -eq $taskName}
