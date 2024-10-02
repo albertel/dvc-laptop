f0c9c537f4e466c9de8b6443e33f1ed064f3b19c
albertel <guy@albertelli.com>
2024-10-02 13:48:16 -0700

    Update Do-LaptopConfigure.ps1

--- Do-LaptopConfigure.ps1
+++ Do-LaptopConfigure.ps1
@@ -7,6 +7,23 @@ Set-StrictMode -version latest
 "Running version 6"
 $branch="main"
 
+$CSharpSig = @’
+[DllImport(“user32.dll”, EntryPoint = “SystemParametersInfo”)]
+public static extern bool SystemParametersInfo(
+             uint uiAction,
+             uint uiParam,
+             uint pvParam,
+             uint fWinIni);
+‘@
+$CursorRefresh = Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo –PassThru
+if ($CursorRefresh::SystemParametersInfo(0x0057,0,$null,0)) {
+	  "Set Cursor"
+} else {
+	"Failed Set Cursor"
+ }
+
+Exit
+
 Function Set-ScreenResolution { 
 
 <# 
