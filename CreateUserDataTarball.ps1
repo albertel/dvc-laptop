73f57aee106acc0c151803bdda1ebcc8d600dc22
albertel <guy@albertelli.com>
2026-07-23 14:48:31 -0700

    Update CreateUserDataTarball.ps1

--- CreateUserDataTarball.ps1
+++ CreateUserDataTarball.ps1
@@ -1,19 +1,11 @@
 cd C:\Users\DVC_volunteer\AppData\Local\Google\Chrome
-tar -c -f C:\Users\DVC_volunteer\Downloads\UserDataStart.tar "User Data"
+tar -c -f C:\Users\DVC_volunteer\Downloads\UserDataStart.tar "User Data\Default\Local Storage\leveldb"
 cd C:\Users\DVC_volunteer\Downloads
 rm -r temp
 mkdir temp
 cd temp
 tar -x -f ..\UserDataStart.tar
-cd "User Data"
-rm -r optimization_guide_model_store
-rm -r GrShaderCache
-rm -r ShaderCache
-rm -r GPUPersistentCache
-rm -r Default/Cache
-rm -r Default/GPUCache
-rm -r Default/DawnGraphiteCache
-rm -r Default/DawnWebGPUCache
-cd ..
+pushd "User Data"
+popd
 tar -c -v -z -f UserData.test.tgz "User Data"
-scp UserData.test.tgz dvc@10.50.7.1:/var/www/html
+scp UserData.test.tgz guy@192.168.1.193:/var/www/html
