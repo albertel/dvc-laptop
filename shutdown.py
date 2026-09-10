5cfed2f669d17073c8e9c15020d4321b2040409a
albertel <guy@albertelli.com>
2026-09-09 18:34:41 -0700

    Create shutdown.py
    Adding a script to shutdown the laptops based on windows remote commands

---
+++ shutdown.py
@@ -0,0 +1,59 @@
+import random
+import threading
+import time
+import winrm
+
+hosts=[
+    "dvc-808",
+] + ["dvc-%02d"%n for n in range(25)]
+
+cmd="ipconfig"
+
+def do_shutdown(host, **kwargs):
+    time.sleep(3*random.random())
+    winrmsession = winrm.Session(host, auth=("PTA_admin", "uni cycle"), transport="ntlm")
+    r=winrmsession.run_cmd(cmd)
+    print("%s\n\n%s\n------------\n%s\n" % (
+        host, r.std_out.decode('ascii'), r.std_err.decode('ascii')))
+    if r.status_code:
+        print("Host %s\nFAILED")
+
+failures={}
+def log_failed_hosts(args):
+    failures[args.thread.name] = "%s" % args.exc_value
+    
+def main():
+    print("Stating Shutdown")
+    threading.excepthook=log_failed_hosts
+    threads = []
+    for host in hosts:
+        threads.append(threading.Thread(
+            target=do_shutdown, name=host, args=(host,)))
+    print("Created")
+
+    for t in threads:
+        t.start()
+    print("Started")
+
+    alive = threads
+    while alive:
+        still_alive = []
+        for t in alive:
+            if t.is_alive():
+                still_alive.append(t)
+            else:
+                t.join()
+                print("Finished ", t.name)
+        alive = still_alive
+        if alive:
+            print("Still running: ", ', '.join(t.name for t in alive))
+        if failures:
+            print("Fail count: ", len(failures.keys()))
+        time.sleep(1)
+
+    if failures:
+        print("Failures")
+        for k in sorted(failures):
+            print("Host %s: %s" % (k, failures[k]))
+                
+main()
